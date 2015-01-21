151ad9b0b9f4eb37013e83fb4b3d97ee60662549
Kyle Heitman <kyle.heitman@uconn.edu>
2015-01-21 15:38:13 -0500

    updated

--- SandBox.R
+++ SandBox.R
@@ -15,6 +15,7 @@ title(main=names(n)[2])
 plot(pcrfit(n[[3]], 1,2,l4))
 title(main=names(n)[3])
 plot(pcrfit(n[[4]], 1,2,l4))
+
 title(main=names(n)[4])
 plot(pcrfit(n[[5]], 1,2,l4))
 title(main=names(n)[5])
@@ -28,33 +29,4 @@ for(k in 2:length(n[[4]])) {
 
 df <- data.frame(matrix(unlist(norms), nrow=30)) #create data frame for pcrfit
 
-fluo1 <- list() #some shitty containers to hold that nonsense
-eff <- list()
-
-for(k in 2:length(df)) { 
-     tryCatch({ 
-     y <- pcrfit(df, 1, k, l4)
-     x <- efficiency(y, plot=FALSE)
-          
-     fluo1[[k-1]] <- x$init1
-     tt <- c(k, "/", length(norms))
-     print(tt)
-     print(x$init1)
-     eff[[k-1]] <- x$eff
-     }, error = function(err) {
-#           y <- pcrfit(df, 1, k+1, l4)
-#           x <- efficiency(y, plot=FALSE)
-#           fluo1[[k-1]] <- as.numeric(x$init1)
-#           print(tt)
-#           print(x$init1)
-     })
-}
-xt <- seq(1, length(unlist(fluo1)))
-
-plot(xt, as.numeric(unlist(fluo1)), ylim=c(-5000,5000), ylab="Initial Fluorescence", xlab="Run Number")
-
-smoothScatter(xt, as.numeric(unlist(fluo1)), ylim=c(-5000,5000), ylab="Initial Fluorescence", xlab="Run Number")
-
-df1 <- data.frame(x=xt, y=as.numeric(unlist(fluo1)))
-ggplot(as.data.frame(df1), aes(x=x, y=y)) + ylab("Initial Template Fluorescence") + xlab("Run Number") + ggtitle("F0 vs Run #") + geom_point(alpha = 0.4) + ylim(c(-5000,5000)) + geom_point()
-
+m2(df) #run the second method with the normalized data
--- m2.R
+++ m2.R
@@ -7,6 +7,46 @@
 #sigmoidal curve type for qPCR analysis, the four-parameter logistic curve (also termed Boltzmann fit)(Boltzmann fit - http://www.graphpad.com/guides/prism/6/curve-fitting/index.htm?reg_classic_boltzmann.htm)
 #additionally its four-parameter log-logistic counterpart to five-parameter versions that were shown to exhibit better fits for asymmetric data
 #choosing the best model from nested f tests
-m2 <- function(gotData,tableNum = 1){
- 
+m2 <- function(df){
+     fluo1 <- list() #initial template fluorescence
+     eff <- list() #efficiency
+     ct <- list() #ct values, 'threshold cycle', according to Guescini
+     r2 <- list() #R^2 value
+     
+     for(k in 2:length(df)) { 
+          tryCatch({ 
+               y <- pcrfit(df, 1, k, l4)
+               x <- efficiency(y, plot=FALSE, type="Cy0") #type = method of efficiency estimation
+               
+               fluo1[[k-1]] <- x$init1
+               tt <- c(k, "/", length(norms))
+               print(tt)        #Cy0 = the intersection of a tangent on the first derivative maximum with the abscissa as calculated according to Guescini
+               eff[[k-1]] <- x$eff
+               ct[[k-1]] <- x$Cy0
+               r2[[k-1]] <- x$Rsq
+          }, error = function(err) {
+          })
+     }
+     
+     m2Print(fluo1, gd, ct, r2)
+     
+     #optional plots
+     # xt <- seq(1, length(unlist(fluo1)))
+     
+     # plot(xt, as.numeric(unlist(fluo1)), ylim=c(-5000,5000), ylab="Initial Fluorescence", xlab="Run Number")
+     # 
+     # smoothScatter(xt, as.numeric(unlist(fluo1)), ylim=c(-5000,5000), ylab="Initial Fluorescence", xlab="Run Number")
+     # 
+     # df1 <- data.frame(x=xt, y=as.numeric(unlist(fluo1)))
+     # ggplot(as.data.frame(df1), aes(x=x, y=y)) + ylab("Initial Template Fluorescence") + xlab("Run Number") + ggtitle("F0 vs Run #") + ylim(c(-5000,5000)) + geom_point(alpha=0.5)
+     
+}
+
+m2Print <- function(fluo, gd, ct, r2) {
+     temp <- list()
+     temp[[1]] <- "Sample, Initial Template Fluorescence, Ct value(according to Guescini), efficiency, R^2 value"
+     for(k in 1:length(fluo)) {
+          temp[[k+1]] <- paste(gd[[1]][[2]][[k+1]][1], gd[[1]][[2]][[k+1]][2], fluo[[k]], ct[[k]], r2[[k]], sep=", ")
+     }
+     writeLines(LOLprint(temp),"test.ddv")
 }
